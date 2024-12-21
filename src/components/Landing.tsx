7d57417e15e428e8b17e92de4257305abd9a6300
Devadutt <devaduttmohancs@gmail.com>
2024-12-21 11:52:52 +0530

    Font added

---
+++ src/components/Landing.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Landing = () => {
+  return (
+    <>
+      <div className="h-screen w-screen bg-white flex items-center">
+        <h1 className='font-Gentona text-[#3a3a3a] text-9xl ml-10'>Phantasia</h1>
+        <Link>
+          <a className='font-Gentona text-[#3a3a3a] text-3xl ml-10'>Login</a>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Landing
