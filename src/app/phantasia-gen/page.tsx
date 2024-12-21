852845b09e10eb02acd92050c48a3a48bcfe84e8
AdithyanA <adithyanayithala@gmail.com>
2024-12-21 15:23:24 +0530

    changes

--- src/app/phantasia-gen/page.tsx
+++ src/app/phantasia-gen/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
     },
     {
       id: 2,
-      name: "Arlene Mccoy",
+      name: "Phil Dunphy",
       avatar:
-        "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+        "https://i.imgur.com/vUvf6bW.jpeg",
     },
   ];
 
