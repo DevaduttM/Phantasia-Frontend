78829539b8382cdc002018bc01e4f577dda875f0
Devadutt <devaduttmohancs@gmail.com>
2024-12-21 12:38:59 +0530

    changes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 const Landing = () => {
   return (
     <>
-      <div className="h-screen w-screen bg-white flex items-center">
-        <h1 className='font-Gentona text-[#3a3a3a] text-[12rem] ml-10'>Phantasia</h1>
-        <Link href='/login' className='absolute font-sans text-lg text-white rounded-[20px] px-8 py-3 bg-black top-3 right-3'>Login</Link>
+      <div className="h-screen w-screen bg-[#ebebeb] flex items-center">
+        <h1 className='font-Gentona text-[#3a3a3a] text-[10rem] ml-10'>Phantasia</h1>
+        <Link href='/login' className='absolute font-sans text-lg text-black rounded-[20px] px-8 py-2 bg-white border-gray-600 border-2 top-3 right-3'>Login</Link>
       </div>
     </>
   )
