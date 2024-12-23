60345176360de77dc747123a4749b1fe0cca4671
AdithyanA <adithyanayithala@gmail.com>
2024-12-23 12:40:56 +0530

    secured api keys

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGL15H9x5OhA0c2TQfiPfgWshY3JNja3s",
-  authDomain: "phantasia-8b888.firebaseapp.com",
-  projectId: "phantasia-8b888",
-  storageBucket: "phantasia-8b888.firebasestorage.app",
-  messagingSenderId: "368090016450",
-  appId: "1:368090016450:web:0d9cb7ea4ec3b9669da95b",
-  measurementId: "G-3M4JH94VHE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
